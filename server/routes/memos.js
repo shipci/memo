2605920af7d5b2000b13fd64eb791d30d041faf6
Ikuo Terado <eqobar@gmail.com>
2013-12-01 22:00:20 +0900

    Update memo when its file is updated

--- server/routes/memos.js
+++ server/routes/memos.js
@@ -1,16 +1,32 @@
+var MEMO_FILE = './memos/memo0.md';
 
 var fs = require('fs');
-var read = fs.createReadStream('./memos/memo0.md');
+var read = fs.createReadStream(MEMO_FILE);
 
 var io = require('socket.io').listen(9001);
 io.sockets.on('connection', function (socket) {
   read.on('data', function (data) {
-    var content = data.toString();
+    startWatching(MEMO_FILE);
 
-    socket.emit('memo', {
-      title: 'Title',
-      content: content
-    });
+    var content = data.toString();
+    sendMemo(content);
+
+    function startWatching (fileName) {
+      var watcher = fs.watch(fileName, {persistent: true}, function (event, filename) {
+        content = fs.readFileSync(MEMO_FILE).toString();
+        sendMemo(content);
+
+        watcher.close();
+        startWatching(fileName);
+      });
+    }
+
+    function sendMemo (content) {
+      socket.emit('memo', {
+        title: 'Title',
+        content: content
+      });
+    }
   });
 });
 
