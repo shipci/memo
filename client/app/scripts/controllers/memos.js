cb6c5adbca31bdbea8d490738df1653d51f9c68a
Ikuo Terado <eqobar@gmail.com>
2013-12-09 02:03:00 +0900

    Show directories with breadcrub

--- client/app/scripts/controllers/memos.js
+++ client/app/scripts/controllers/memos.js
@@ -10,6 +10,23 @@ angular.module('memoApp')
     $scope.dir = 'memos' + ($routeParams.dir ? '/' + $routeParams.dir : '');
     // console.log($scope.dir);
 
+    var dirSplit = [];
+    var fullDir = '';
+    var dirs = $scope.dir.split('/');
+    var length = dirs.length;
+    for (var i = 0; i < length; i++) {
+      var dir = {
+        name: dirs[i]
+      }
+      if (i !== length - 1) {
+        dir.link = fullDir + dirs[i];
+      }
+      dirSplit.push(dir);
+      fullDir += dirs[i] + '/';
+    }
+    $scope.dirSplit = dirSplit;
+    console.log($scope.dirSplit);
+
     $scope.memos = [];
 
     $http.get($scope.dir).success(function (data) {
