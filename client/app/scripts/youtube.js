0c33bc770456f777d4cfd220e43a4d0bbc9b1320
Ikuo Terado <eqobar@gmail.com>
2013-12-14 02:47:41 +0900

    Support YouTube width and height to be specified

--- client/app/scripts/youtube.js
+++ client/app/scripts/youtube.js
@@ -10,16 +10,29 @@
         type: 'lang',
         regex: '\\^\\^([\\S]+)',
         replace: function(match, url) {
-          var m, video_id, youtube;
-          youtube = /^.*((youtube\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-          if (youtube.test(url)) {
-            m = url.match(youtube);
-            if (m && m[7].length === 11) {
-              video_id = m[7];
-              return '<iframe src="//www.youtube.com/embed/' + video_id + '?rel=0" frameborder="0" allowfullscreen></iframe>';
-            } else {
-              return match;
+          var videoId = null;
+          var width = 640;
+          var height = 390;
+
+          var youtubeWH = /^\[(\d+)x(\d+)\].*((youtube\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+          var youtube   = /^.*((youtube\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+
+          if (youtubeWH.test(url)) {
+            var m = url.match(youtubeWH);
+            if (m && m[9].length === 11) {
+              width = m[1];
+              height = m[2];
+              videoId = m[9];
             }
+          } else if (youtube.test(url)) {
+            var m = url.match(youtube);
+            if (m && m[7].length === 11) {
+              videoId = m[7];
+            }
+          }
+
+          if (videoId) {
+            return '<iframe width="' + width + '" height="' + height + '" src="//www.youtube.com/embed/' + videoId + '?rel=0" frameborder="0" allowfullscreen></iframe>';
           } else {
             return match;
           }
