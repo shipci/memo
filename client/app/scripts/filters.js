f37ee0fcfc8184fafaa750afd5a3bf9bce42f15b
Ikuo Terado <eqobar@gmail.com>
2014-02-25 00:18:58 +0900

    Use marked for markdown formatter instead of showdown

--- client/app/scripts/filters.js
+++ client/app/scripts/filters.js
@@ -4,13 +4,16 @@
 
 angular.module('memoFilters', [])
   .filter('markdown', function ($sce) {
-    var converter = new Showdown.converter({ extensions: ['github', 'youtube'] });
+    // var converter = new Showdown.converter({ extensions: ['github', 'youtube'] });
+    var marked = window.marked;
+
     return function (input) {
       if (!input) {
         return null;
       }
 
       // console.log(input);
-      return $sce.trustAsHtml(converter.makeHtml(input));
+      // return $sce.trustAsHtml(converter.makeHtml(input));
+      return $sce.trustAsHtml(marked(input));
     };
   });
