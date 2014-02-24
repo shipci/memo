417c81d427178f6eabbb1764ae69668b297afab5
Ikuo Terado <eqobar@gmail.com>
2014-02-25 01:57:44 +0900

    Refactor code for rss configuration

---
+++ server/config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.rssConfig = {
+  title: 'memo blog',
+  description: 'memo blog',
+  author: 'Ikuo Terado',
+  feed_url: 'http://example.com/rss.xml',
+  site_url: 'http://example.com',
+  image_url: 'http://example.com/icon.png',
+  docs: 'http://example.com/rss/docs.html',
+  language: 'en',
+  copyright: '2014 Ikuo Terado',
+};
