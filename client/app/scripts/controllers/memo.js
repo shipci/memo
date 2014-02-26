8a20ad1bf32fccb7356135822025ccefd7c515a0
Ikuo Terado <eqobar@gmail.com>
2014-02-27 01:06:53 +0900

    Set default title and focus for publish dialog

--- client/app/scripts/controllers/memo.js
+++ client/app/scripts/controllers/memo.js
@@ -29,6 +29,30 @@ angular.module('memoApp')
       $scope.$apply();
     });
 
+    $('.modal').on('show.bs.modal', function (e) {
+      // Get the first line in the content as a candidate of content's title
+      var content = $scope.memo.content;
+      var contentLines = content.split('\n');
+      var title = '';
+      for (var i = 0; i < contentLines.length; i++) {
+        if (contentLines[i]) {
+          title = contentLines[i];
+          break;
+        }
+      }
+
+      // Remove '#' and spaces
+      title = title.replace(/#+\s*/, '');
+      // console.log(title);
+
+      $scope.memoTitle = title;
+      $scope.$apply();
+    });
+
+    $('.modal').on('shown.bs.modal', function (e) {
+      $(this).find('input').focus().select();
+    });
+
     $scope.publish = function () {
       // console.log('publish');
 
