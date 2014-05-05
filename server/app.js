e44a9e0203e002883d2aef1bd3c03af3facb7083
Ikuo Terado <eqobar@gmail.com>
2014-05-06 04:13:55 +0900

    Update express to 4.x

--- server/app.js
+++ server/app.js
@@ -1,37 +1,29 @@
-'use strict';
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-var routes = require('./routes');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
 var memos = require('./routes/memos');
 var rss = require('./routes/rss');
-var http = require('http');
-var path = require('path');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 10001);
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-// app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+
+app.use(favicon());
+// app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-}
-
 app.get('/', routes.index);
-app.get('/memos/*', memos.list);
+app.get('/memos/:dir', memos.list);
 app.get('/memos', memos.list);
 app.post('/memos/*', memos.create);
 app.put('/memos/*', memos.rename);
@@ -39,10 +31,38 @@ app.get('/files/*', memos.get);
 app.get('/rss/memo.rdf', rss.get);
 app.post('/rss/*', rss.post);
 
-var server = http.createServer(app);
-server.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-var io = require('socket.io').listen(server, {'log level': 0});
-memos.start(io);
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.memos = memos;
+
+
+module.exports = app;
