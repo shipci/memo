f0f0bebe894bb5375209a2a79e4e3759946d0831
Ikuo Terado <eqobar@gmail.com>
2014-05-06 05:51:08 +0900

    Minor change

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,6 @@
 'use strict';
 
-var publicPath = require('path').join(__dirname, '../public/');
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
+  var publicPath = require('path').join(__dirname, '../public/');
   res.sendfile(publicPath + 'index.html');
 };
