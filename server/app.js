feefd62c4d5c55110ca6aa00aff8b2838ce529bf
Ikuo Terado <eqobar@gmail.com>
2014-05-16 02:06:22 +0900

    Support request logs to be saved into file

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
-// var logger = require('morgan');
+var log4js = require('log4js');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -19,7 +19,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(favicon());
-// app.use(logger('dev'));
+
+// Log
+log4js.configure(path.join(__dirname, 'log4js_setting.json'));
+var loggerRequest = log4js.getLogger('request');
+app.use(log4js.connectLogger(loggerRequest, { level: log4js.levels.INFO }));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
